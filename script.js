723096385f615d35a157715ba93d680669826bbc
pheeliyan <tehila2000hero@gmail.com>
2026-07-14 11:58:48 +0100

    sstarte learning javascript

---
+++ script.js
@@ -0,0 +1,121 @@
+// var name = 'Tehila'
+// var name = 'john'
+
+// console.log(name)
+//const name = 'Tehila'
+//const age = 13
+    // const is_student = true
+    // const subjects = ['html', 'js', 'css']
+    // const student = {
+    //     name: 'Tehila',
+    //     age: 14,
+    //     enrolled: false
+    // }
+
+    // console.log(name)
+    // console.log(age)
+    // console.log(is_student)
+    // console.log(subjects)
+    // console.log(student.name)
+
+// let a = 20;
+// let b = 6;
+
+// console.log(a ** b)
+ 
+// let score = 10
+// score--
+// console.log(score)
+
+// let price = 100
+// price -= 50
+// console.log(price)
+
+// let a = 10
+// let b = 20
+
+// console.log(a <= b)
+
+// let maths = 300
+// let english = 500
+// let geography = 200
+
+// let score = maths + english + geography
+
+// console.log(score)
+
+// let total = score / 3
+
+// console.log(total)
+
+// let age = -20 
+// if(age>0) {
+//     console.log('positive')
+// }
+// else{
+//     console.log('negetive')
+// }
+
+// let age = 7
+// if(age<=0){
+//     console.log('enter a valid age')
+// }
+// else if(age>=18){
+//     console.log('you are eligible to vote')
+// }
+// else{
+//     console.log('you are too young to vote')
+// }
+
+// let temperature = 50
+// if(temperature<30){
+//     console.log(`the weather is cold ${temperature}f`)
+// }
+// else{
+//     console.log(`the weather is hot ${temperature}f`)
+// }
+
+// let score = 5
+// if(score>=70){
+//     console.log('A')
+// }
+// else if(score>=60 ){
+//     console.log('B')
+// }
+// else if(score>=50 ){
+//     console.log('C')
+// }
+// else if(score>=40){
+//     console.log('D')
+// }
+// else{
+//     console.log('F')
+// }
+
+// let m = 22
+// let h = 45
+
+// if (m > h){
+//     console.log(m +' is greater')
+// }
+// else if (h > m){
+//     console.log(h + ' is greater')
+// }
+// else if (m < h){
+//     console.log(m -'is less')
+// }
+// else if (h < m){
+//     console.log(h -'is less')
+// }
+// else{
+//     console.log('they are equal')
+// }
+const greeting = 'Good Afternoon'
+const name = 'Tehila'
+const tehila = document.querySelector('#greet')
+
+if (tehila) {
+    greet.addEventListener('click', function () {
+        alert(greeting + name)
+    })
+}
